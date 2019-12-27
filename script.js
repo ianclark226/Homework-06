2b99855a169cf33b45a5f7ec0bde560bf1051385
Ian Clark <ianclark226@gmail.com>
2019-12-27 13:30:18 -0500

    12/27

--- script.js
+++ script.js
@@ -26,13 +26,14 @@ $(document).ready(function(){
 });
 
 function show(data){
-    return "<h3><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" +
-    "<h3><strong>Description</strong>: "+ data.weather[0].description +"</h3>" +
-    "<h3><strong>Temperature</strong>: " + data.main.temp + "</h3>" +
-    "<h3><strong>Pressure</strong>: " + data.main.pressure + "</h3>" +
-    "<h3><strong>Humidity</strong>: " + data.main.humidity + "</h3>" +
-    "<h3><strong>Min. Temperature</strong>: " + data.main.temp_min + "</h3>" +
-    "<h3><strong>Max. Temperature</strong>: " + data.main.temp_max + "</h3>" +
-    "<h3><strong>Wind Speed</strong>: " + data.wind.speed + "</h3>" +
-    "<h3><strong>Wind-Direction</strong>: " + data.wind.deg + "</h3>";
+    return "<h3 style='font-size:40px; font-weight: bold;'>Current Weather for " +data.name + ", " + data.sys.country +"</h2>" +
+    "<h3 style='padding-left:40px;'><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" +
+    "<h3><strong>Description</strong>: <img src=http://openweathermap.org/img/w/ "+ data.weather[0].description +"</h3>" +
+    "<h3><strong>Temperature</strong>: " + data.main.temp + "&deg;C</h3>" +
+    "<h3><strong>Pressure</strong>: " + data.main.pressure + "hPa</h3>" +
+    "<h3><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
+    "<h3><strong>Min. Temperature</strong>: " + data.main.temp_min + "&deg;C</h3>" +
+    "<h3><strong>Max. Temperature</strong>: " + data.main.temp_max + "&deg;C</h3>" +
+    "<h3><strong>Wind Speed</strong>: " + data.wind.speed + "m/s</h3>" +
+    "<h3><strong>Wind-Direction</strong>: " + data.wind.deg + "&deg;</h3>";
 }
