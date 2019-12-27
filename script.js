aa2eeb4b7daf1b46d2f44444ab2792f3d26e6b5d
Ian Clark <ianclark226@gmail.com>
2019-12-27 13:16:30 -0500

    12/27

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ $(document).ready(function(){
 
             $.ajax({
 
-                url: "",
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "APPID=",
+                type: "GET",
+                dataType: "jsonp",
+                sucess: function(data) {
+                    var widget = show(data);
+
+                    $("#show").html(widget);
+
+                    $("#city").val('');
+                }
             })
 
         }else{
@@ -15,3 +24,15 @@ $(document).ready(function(){
         }
     });
 });
+
+function show(data){
+    return "<h3><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" +
+    "<h3><strong>Description</strong>: "+ data.weather[0].description +"</h3>" +
+    "<h3><strong>Temperature</strong>: " + data.main.temp + "</h3>" +
+    "<h3><strong>Pressure</strong>: " + data.main.pressure + "</h3>" +
+    "<h3><strong>Humidity</strong>: " + data.main.humidity + "</h3>" +
+    "<h3><strong>Min. Temperature</strong>: " + data.main.temp_min + "</h3>" +
+    "<h3><strong>Max. Temperature</strong>: " + data.main.temp_max + "</h3>" +
+    "<h3><strong>Wind Speed</strong>: " + data.wind.speed + "</h3>" +
+    "<h3><strong>Wind-Direction</strong>: " + data.wind.deg + "</h3>";
+}
