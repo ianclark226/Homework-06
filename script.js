4c48be96ba15e6cea4878dcd5306004fb7079334
Ian Clark <ianclark226@gmail.com>
2020-01-03 12:34:44 -0500

    1/3

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ $(document).ready(function(){
 
             $.ajax({
 
-                url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&APPID=49034a6efdfe8d07c20b64468ee472fe",
+                url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&APPID=49034a6efdfe8d07c20b64468ee472fe",
                 type: "GET",
                 dataType: "jsonp",
                 success: function(data) {
                     var widget = show(data);
                     console.log(data);
+                 
 
                     $("#show").html(widget);
 
@@ -30,12 +31,13 @@ $(document).ready(function(){
 function show(data){
     return "<h3 style='font-size:40px; font-weight: bold;'>Current Weather for " +data.name + ", " + data.sys.country +"</h2>" +
     "<h3 style='padding-left:40px;'><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" +
-    "<h3 style='padding-left:40px;'><strong>Description</strong>: <img src=http://openweathermap.org/img/w/ "+ data.weather[0].description +"</h3>" +
-    "<h3 style='padding-left:40px;'><strong>Temperature</strong>: " + data.main.temp + "&deg;C</h3>" +
+    "<h3 style='padding-left:40px;'><strong>Description</strong>: <img src='http://openweathermap.org/img/wn/ "+ data.weather[0].icon +".png'>"+ data.weather[0].description+ "</h3>" +
+    "<h3 style='padding-left:40px;'><strong>Temperature</strong>: " + data.main.temp + "&deg;F</h3>" +
     "<h3 style='padding-left:40px;'><strong>Pressure</strong>: " + data.main.pressure + "hPa</h3>" +
     "<h3 style='padding-left:40px;'><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
-    "<h3 style='padding-left:40px;'><strong>Min. Temperature</strong>: " + data.main.temp_min + "&deg;C</h3>" +
-    "<h3 style='padding-left:40px;'><strong>Max. Temperature</strong>: " + data.main.temp_max + "&deg;C</h3>" +
+    "<h3 style='padding-left:40px;'><strong>Min. Temperature</strong>: " + data.main.temp_min + "&deg;F</h3>" +
+    "<h3 style='padding-left:40px;'><strong>Max. Temperature</strong>: " + data.main.temp_max + "&deg;F</h3>" +
     "<h3 style='padding-left:40px;'><strong>Wind Speed</strong>: " + data.wind.speed + "m/s</h3>" +
-    "<h3 style='padding-left:40px;'><strong>Wind-Direction</strong>: " + data.wind.deg + "&deg;</h3>";
+    "<h3 style='padding-left:40px;'><strong>Wind-Direction</strong>: " + data.wind.deg + "&deg;</h3>" +
+    "<h3 style='padding-left:40px;'><strong>UV-Index</strong>: " + data.main.uv + "&deg;</h3>";
 }
